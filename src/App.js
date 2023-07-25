39d71c4d25ec5d0feb68b4577dcd1e801517f7d8
ogutierrezh <oscaralonsog@gmail.com>
2023-07-24 19:29:22 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CONTENIDO DEL CURSO</h1>
+      <p> INTRODUCCION A HTML </p>
+      <p> HTML Y CSS</p>
+      <p> SELECTORES CSS </p>
+      <p> BOX MODEL</p>
+      <p> MEDIA QUERIES </p>
+      <p> FLEX BOX</p>
+      <p> CSS GRID</p>
+      <p>  FRAMEWORK BOOTSTRAP</p>
+      <p> REACT</p>
+      
+    </div>
+  );
+}
+
+export default App;
